85a30dc7dc70765007792c598bf4a83b5a8b96ba
PRASASTIRAI <77830876+PRASASTIRAI@users.noreply.github.com>
2021-05-10 18:02:24 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -8,23 +8,23 @@ menu_list_array =[
 
 function getmenu(){
     var htmldata;
-    htmldata ="<o1 class='menulist'>"
+    htmldata ="<ol class='menulist'>"
     menu_list_array.sort();
     for(var i=0;i<menu_list_array.length;i++){
         htmldata=htmldata+'<li>'+ menu_list_array[i] +'<li>'
     }
-    htmldata=htmldata+'</o1>'
+    htmldata=htmldata+'</ol>'
     document.getElementById("display_menu").innerHTML= htmldata;
 }
 
-function addmenu(){
+function add_item(){
     htmldata;
     item=document.getElementById("add_item").value
     menu_list_array.push(item);
     menu_list_array.sort();
-    htmldata="<section class='card'>"
+    htmldata="<section class='cards'>"
     for(i=0;i<menu_list_array.length;i++){
-    htmldata='<div class="card">' + '<img src="images/pizza.png"/>' + menu_list_array[i] + '</div>'
+    htmldata=htmldata + '<div class="cards">' + '<img src="pizza.png"/>' + menu_list_array[i] + '</div>'
 }
 htmldata=htmldata + "</section>"
 document.getElementById("display_addedmenu").inner= htmldata;
