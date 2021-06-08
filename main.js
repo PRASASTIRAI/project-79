58c843a8196f49719a02d5b0c4d2b8dc0ed65c08
PRASASTIRAI <77830876+PRASASTIRAI@users.noreply.github.com>
2021-06-08 21:06:52 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -16,13 +16,13 @@ function getmenu(){
     for(var i=0;i<menu_list_array.length;i++){
         var htmldata=htmldata+'<li>'+ menu_list_array[i] +'<li>'
     }
-    htmldata=htmldata+'</ol>'
+    htmldata=htmldata+"</ol>"
     document.getElementById("display_menu").innerHTML= htmldata;
 }
 
 function add_item(){
-    htmldata;
-    item=document.getElementById("add_item").value
+    var htmldata;
+    var item=document.getElementById("add_item").value
     menu_list_array.push(item);
     menu_list_array.sort();
     htmldata="<section class='cards'>"
